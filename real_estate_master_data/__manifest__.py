b559b417e2b0b88ebe8df381900c7990d4f75fbf
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-07-07 21:05:00 +0200

    Add files via upload

---
+++ real_estate_master_data/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Real Estate - master data",
+    'version': "14.0.1.1",
+    'author':'Pre Sys ',
+    'category': "Website",
+    'summary': '',
+    'description': '''
+
+        ''',
+    'sequence': 14,
+    'depends' : ['itsys_real_estate'],
+    'data' : [
+       
+        'view/crm_team_view.xml',
+      
+              ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'license': "AGPL-3",
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ real_estate_master_data/models/crm_team.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#from openerp import models, fields, api
+from odoo import models, fields, api
+
+
+class Team(models.Model):
+    _inherit = 'product.template'
+
+  
