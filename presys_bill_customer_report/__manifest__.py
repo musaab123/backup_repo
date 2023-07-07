a79199f12e6bb08726cec34597362d7737576e4c
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-07-07 20:44:06 +0200

    Add files via upload

---
+++ presys_bill_customer_report/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    NCTR, Nile Center for Technology Research
+#    Copyright (C) 2021-2022 NCTR (<http://www.nctr.sd>).
+#
+##############################################################################
+{
+    "name": " Detailed Sales  Report",
+    "version": "1.1",
+    "category": "Generic Modules/sale",
+    "description": """ Manage sale module
+
+    """,
+    "author": "NCTR",
+    "website": "http://www.nctr.sd",
+    "depends":['point_of_sale','web'],
+    "data": [
+
+        'reports/sale_header_custom.xml',
+        'reports/jornal_entry.xml',
+        'reports/report.xml',
+
+        
+        
+
+    
+    ],
+    "installable": True,
+    "active": True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
