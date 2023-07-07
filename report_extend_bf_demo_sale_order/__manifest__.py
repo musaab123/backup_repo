dee037a056cb4384c55b20e9e6ac7652a991a3fb
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-07-07 21:09:36 +0200

    Add files via upload

---
+++ report_extend_bf_demo_sale_order/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Report Templates Demo Sale Order',
+    'description': 'Report Templates Demo Sale Order',
+    'summary': 'Report Templates Demo Sale Order',
+    'category': 'All',
+    'version': '1.0',
+    'website': 'http://www.build-fish.com/',
+    "license": "AGPL-3",
+    'author': 'BuildFish',
+    'depends': [
+        'report_extend_bf',
+        'report_extend_bf_sale_base',
+        "sale",
+        "sale_management",
+    ],
+    'data': [
+        'data/templates.xml',
+        'report.xml',
+    ],
+    'live_test_url': 'http://report_extend_bf.odoo15.build-fish.com',
+    'images': ['static/description/banner.png'],
+    'application': True,
+}
